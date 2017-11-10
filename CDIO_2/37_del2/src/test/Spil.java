2bd0d17bf2f7e8a4eb54fed37cf0d828e3e6f0ee
Adam <Adam@DESKTOP-IK5I8ON.eduroam.wireless.dtu.dk>
2017-11-10 12:45:16 +0100

    Opdateret spilklasse

--- CDIO_2/37_del2/src/test/Spil.java
+++ CDIO_2/37_del2/src/test/Spil.java
@@ -1,1 +1,49 @@
+package test;
+
+import java.io.IOException; 
+
+import desktop_resources.GUI;
+import spil.Bræt;
+import spil.Spiller;
+import spil.Sprog;
+import spil.Tur;
+
+public class Spil {
+
+	public static void main(String[] args) {
+		Bræt bræt = new Bræt();
+		bræt.lavBræt();
+		Sprog sp = new Sprog();
+		try {
+			sp.tekst("dansk.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Spiller[] spillerArray = new Spiller[2];
+
+		for(int i=0; i<spillerArray.length; i++){
+			int startbeløb = 1000;
+			String s = GUI.getUserString(sp.getPlayerName() +" "+ (i+1));
+			if(s.equals(""))
+				s = sp.getPlayer() +" " + (i+1);
+			spillerArray[i] = new Spiller(s,startbeløb);
+			GUI.addPlayer(s, startbeløb);
+		}
+		
+		boolean fundetVinder = false;
+		while (fundetVinder == false) {
+			for(int j = 0; spillerArray.length>j; j++) {
+				spillerArray[j].brugTur(new Tur(spillerArray[j].getNavn()));
+
+				fundetVinder = spillerArray[j].isVinder();
+				if (fundetVinder == true)
+					break;
+			}
+
+		}
+
+	}
+
+}
 
